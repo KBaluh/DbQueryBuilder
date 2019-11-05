f6b5d710e27571d5d4f4dca9d640d43d688839c4
Balukh Konstantin <baluh.kostya@gmail.com>
2019-11-05 20:51:22 +0200

    IQueryQuotes interface

---
+++ DbQueryBuilder/Queries/IQueryQuotes.cs
@@ -0,0 +1,17 @@
+﻿namespace DbQueryBuilder.Queries
+{
+    public interface IQueryQuotes
+    {
+        /// <summary>
+        /// Each database has own quotes for insert e.g. " or '
+        /// </summary>
+        /// <returns></returns>
+        string GetInsertFieldQuote();
+
+        /// <summary>
+        /// Each database has own quetes for field incapsulations e.g. " or '
+        /// </summary>
+        /// <returns></returns>
+        string GetFieldQuote();
+    }
+}
