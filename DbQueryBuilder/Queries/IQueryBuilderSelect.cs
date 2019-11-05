5a202a9b6fac6607ffc3a0739af766e73f724c5d
Balukh Konstantin <baluh.kostya@gmail.com>
2019-11-05 21:51:46 +0200

    QueryBuilderSelect implementation

---
+++ DbQueryBuilder/Queries/IQueryBuilderSelect.cs
@@ -0,0 +1,49 @@
+﻿namespace DbQueryBuilder.Queries
+{
+    public interface IQueryBuilderSelect
+    {
+        /// <summary>
+        /// Columns for selecting.
+        /// Example without alias: "Customer.Name"
+        /// Example with alias: "Customer.Name AS 'CustomerName'"
+        /// Example without table name: "Name"
+        /// Example without table name with alias: "Name AS 'CustomerName'
+        /// If you don't pass any arguments it will select all columns *
+        /// </summary>
+        /// <param name="columns"></param>
+        void Select(string[] columns);
+
+        /// <summary>
+        /// Sql block FROM
+        /// </summary>
+        /// <param name="tableName"></param>
+        void From(string tableName);
+
+        /// <summary>
+        /// Where with customer filterin condition
+        /// </summary>
+        /// <param name="customWhere">any SQL filter</param>
+        void Where(string customWhere);
+
+        /// <summary>
+        /// Adding where builder into inner where filters
+        /// </summary>
+        /// <param name="where"></param>
+        void Where(QueryBuilderWhere where);
+
+        /// <summary>
+        /// Adding condition of Where block
+        /// </summary>
+        /// <param name="whereField">Field for filtering</param>
+        /// <param name="whereValue">Value</param>
+        QueryBuilderWhere Where(string whereField, object whereValue);
+
+        /// <summary>
+        /// Adding condition of Where block with custom operand
+        /// </summary>
+        /// <param name="whereField">Field for filtering</param>
+        /// <param name="operand">Operand</param>
+        /// <param name="whereValue">Value</param>
+        QueryBuilderWhere Where(string whereField, string operand, object whereValue);
+    }
+}
--- DbQueryBuilder/Queries/QueryBuilderSelect.cs
+++ DbQueryBuilder/Queries/QueryBuilderSelect.cs
@@ -4,11 +4,19 @@ using DbQueryBuilder.Databases;
 
 namespace DbQueryBuilder.Queries
 {
-    public class QueryBuilderSelect
+    public class QueryBuilderSelect : IQueryBuilderSelect
     {
         #region Database fields
         private IDatabase Database { get; }
         private IQueryQuotes QueryQuotes => Database.GetQueryQuotes();
+        #endregion
+
+        #region Select
+
+        private string _select = " * ";
+
+        private string _from = string.Empty;
+
         #endregion
 
         #region Where
@@ -28,6 +36,58 @@ namespace DbQueryBuilder.Queries
             Database = database ?? throw new System.ArgumentNullException(nameof(database));
             _builderWhere = new QueryBuilderWhere(QueryQuotes);
             _builderWhere.And(_builderWhere);
+        }
+
+        #endregion
+
+        #region Implementation of IQueryBuilderSelect
+
+        public void Select(string[] columns)
+        {
+            if (columns == null || columns.Length == 0)
+            {
+                _select = " * ";
+            }
+            else
+            {
+                string columnString = string.Join(", ", columns);
+                _select = columnString;
+            }
+        }
+
+        public void From(string tableName)
+        {
+            _from = tableName;
+        }
+
+        public void Where(string customWhere)
+        {
+            _customWhere = customWhere;
+        }
+        
+        public void Where(QueryBuilderWhere where)
+        {
+            int index = _builderWheres.IndexOf(where);
+            if (index == -1)
+            {
+                _builderWheres.Add(where);
+            }
+            else
+            {
+                _builderWheres[index] = where;
+            }
+        }
+
+        public QueryBuilderWhere Where(string whereField, object whereValue)
+        {
+            _builderWhere.Where(whereField, whereValue);
+            return _builderWhere;
+        }
+
+        public QueryBuilderWhere Where(string whereField, string operand, object whereValue)
+        {
+            _builderWhere.Where(whereField, operand, whereValue);
+            return _builderWhere;
         }
 
         #endregion
