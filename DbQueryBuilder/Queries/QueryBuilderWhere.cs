315d19611370362f70fa9feaff567860f1b9c0d0
Balukh Konstantin <baluh.kostya@gmail.com>
2019-11-05 20:53:55 +0200

    QueryBuilderWhere

---
+++ DbQueryBuilder/Queries/QueryBuilderWhere.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DbQueryBuilder.Queries
+{
+    /// <summary>
+    /// Query builder for block WHERE in SQL
+    /// </summary>
+    public class QueryBuilderWhere
+    {
+        private readonly IQueryQuotes _quotes;
+
+        public QueryBuilderWhere(IQueryQuotes quotes)
+        {
+            _quotes = quotes ?? throw new ArgumentNullException("quotes");
+        }
+    }
+}
