88edc538d9a327c5048b2e7b9f1e50bf015fb4ad
Balukh Konstantin <baluh.kostya@gmail.com>
2019-11-05 20:49:00 +0200

    QueryBuilderSelect

---
+++ DbQueryBuilder/Queries/QueryBuilderSelect.cs
@@ -0,0 +1,14 @@
+﻿using DbQueryBuilder.Databases;
+
+namespace DbQueryBuilder.Queries
+{
+    public class QueryBuilderSelect
+    {
+        private IDatabase Database { get; }
+
+        public QueryBuilderSelect(IDatabase database)
+        {
+            Database = database;
+        }
+    }
+}
