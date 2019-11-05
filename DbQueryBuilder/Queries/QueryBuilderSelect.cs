b51985b7f37b40943c68f18ee3c39e4dd1028731
Balukh Konstantin <baluh.kostya@gmail.com>
2019-11-05 21:10:34 +0200

    Initialization for Select builder

--- DbQueryBuilder/Queries/QueryBuilderSelect.cs
+++ DbQueryBuilder/Queries/QueryBuilderSelect.cs
@@ -1,14 +1,35 @@
-﻿using DbQueryBuilder.Databases;
+﻿using System.Collections.Generic;
+
+using DbQueryBuilder.Databases;
 
 namespace DbQueryBuilder.Queries
 {
     public class QueryBuilderSelect
     {
+        #region Database fields
         private IDatabase Database { get; }
+        private IQueryQuotes QueryQuotes => Database.GetQueryQuotes();
+        #endregion
+
+        #region Where
+
+        private string _customWhere = "";
+
+        private readonly QueryBuilderWhere _builderWhere;
+
+        private readonly List<QueryBuilderWhere> _builderWheres = new List<QueryBuilderWhere>();
+
+        #endregion
+
+        #region Ctor
 
         public QueryBuilderSelect(IDatabase database)
         {
-            Database = database;
+            Database = database ?? throw new System.ArgumentNullException(nameof(database));
+            _builderWhere = new QueryBuilderWhere(QueryQuotes);
+            _builderWhere.And(_builderWhere);
         }
+
+        #endregion
     }
 }
