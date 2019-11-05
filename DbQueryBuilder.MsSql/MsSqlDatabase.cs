acd4dbf8cbe2d9b55acdadd0b9f01f54fd2cba9f
Balukh Konstantin <baluh.kostya@gmail.com>
2019-11-05 23:03:54 +0200

    Implementation of IDatabase

--- DbQueryBuilder.MsSql/MsSqlDatabase.cs
+++ DbQueryBuilder.MsSql/MsSqlDatabase.cs
@@ -1,12 +1,21 @@
-﻿using System.Data;
+﻿using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace DbQueryBuilder.MsSql
 {
     public class MsSqlDatabase : IDatabase
     {
+        #region Private fields
         private SqlConnection _connection;
         private readonly string _connectionString;
+
+        private readonly Dictionary<IDataReader, SqlConnection> _readerConnections = new Dictionary<IDataReader, SqlConnection>();
+
+        internal IQueryFactory QueryFactory { get; }
+        #endregion
+
+        #region Ctor
 
         public MsSqlDatabase(string connectionString)
         {
@@ -15,9 +24,11 @@ namespace DbQueryBuilder.MsSql
             QueryFactory = new MsSqlQueryFactory(this);
         }
 
-        public IQueryFactory QueryFactory { get; }
+        #endregion
 
-        public void Connnect()
+        #region IDatabase implementation
+
+        public void Connect()
         {
             if (_connection == null)
             {
@@ -42,6 +53,94 @@ namespace DbQueryBuilder.MsSql
             }
         }
 
+        public IDataReader ExecuteResultQuery(IQueryBuilderSelect queryBuilder)
+        {
+            return ExecuteResultQuery(queryBuilder.ToString());
+        }
+
+        public void CloseDataReader(IDataReader dataReader)
+        {
+            _readerConnections.TryGetValue(dataReader, out SqlConnection connection);
+            if (connection != null)
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                }
+            }
+            _readerConnections.Remove(dataReader);
+            dataReader.Close();
+            dataReader.Dispose();
+        }
+
+        public IDataAdapter GetDataAdapter(IQueryBuilderSelect queryBuilder)
+        {
+            return GetDataAdapter(queryBuilder.ToString());
+        }
+
+        public IDbCommand CreateCommand()
+        {
+            IDbCommand command = new SqlCommand();
+            return command;
+        }
+
+        public IDataParameter CreateParameter()
+        {
+            IDataParameter parameter = new SqlParameter();
+            return parameter;
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private IDataReader ExecuteResultQuery(string query)
+        {
+            SqlConnection connection = new SqlConnection(GetConnectionString());
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = query;
+            IDataReader dataReader = command.ExecuteReader();
+            _readerConnections.Add(dataReader, connection);
+            return dataReader;
+        }
+
+        private bool ExecuteQuery(IDbCommand command)
+        {
+            command.Connection = _connection;
+            SqlTransaction transaction = _connection.BeginTransaction();
+            command.Transaction = transaction;
+            try
+            {
+                command.ExecuteNonQuery();
+                transaction.Commit();
+                return true;
+            }
+            catch (SqlException)
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+
+        private bool ExecuteNonQuery(IDbCommand command)
+        {
+            command.Connection = _connection;
+            command.ExecuteNonQuery();
+            return true;
+        }
+
+        private IDataAdapter GetDataAdapter(string query)
+        {
+            if (_connection == null)
+            {
+                Connect();
+            }
+            IDataAdapter dataAdapter = new SqlDataAdapter(query, _connection);
+            return dataAdapter;
+        }
+
         private SqlConnection CreateConnection()
         {
             string connectionString = GetConnectionString();
@@ -52,5 +151,7 @@ namespace DbQueryBuilder.MsSql
         {
             return _connectionString;
         }
+
+        #endregion
     }
 }
--- DbQueryBuilder/IDatabase.cs
+++ DbQueryBuilder/IDatabase.cs
@@ -1,13 +1,21 @@
-﻿namespace DbQueryBuilder
+﻿using System.Data;
+
+namespace DbQueryBuilder
 {
     /// <summary>
     /// Database query executer
     /// </summary>
     public interface IDatabase
     {
-        IQueryFactory QueryFactory { get; }
-
-        void Connnect();
+        void Connect();
         void Disconnect();
+
+        IDataReader ExecuteResultQuery(IQueryBuilderSelect queryBuilder);
+        void CloseDataReader(IDataReader dataReader);
+
+        IDataAdapter GetDataAdapter(IQueryBuilderSelect queryBuilder);
+
+        IDbCommand CreateCommand();
+        IDataParameter CreateParameter();
     }
 }
