f1c39866ded88ea9ed3c63d2e59867df12b67d0c
Balukh Konstantin <baluh.kostya@gmail.com>
2019-11-05 20:36:43 +0200

    Abstract database query executer

---
+++ DbQueryBuilder/Databases/Database.cs
@@ -0,0 +1,31 @@
+﻿namespace DbQueryBuilder.Databases
+{
+    /// <summary>
+    /// Database query executer
+    /// </summary>
+    public abstract class Database : IDatabase
+    {
+        #region Properties
+
+        public DbType DatabaseType { get; }
+
+        #endregion
+
+        #region Ctor
+
+        protected Database(DbType dbType)
+        {
+            DatabaseType = dbType;
+        }
+
+        #endregion
+
+        #region Implementation of IDatabase
+
+        public abstract void Connnect();
+
+        public abstract void Disconnect();
+
+        #endregion
+    }
+}
---
+++ DbQueryBuilder/Databases/IDatabase.cs
@@ -0,0 +1,11 @@
+﻿namespace DbQueryBuilder.Databases
+{
+    /// <summary>
+    /// Database query executer
+    /// </summary>
+    public interface IDatabase
+    {
+        void Connnect();
+        void Disconnect();
+    }
+}
