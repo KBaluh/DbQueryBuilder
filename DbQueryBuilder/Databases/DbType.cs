38fd9efb9c62632daa1976a33fc818b7dbdd9cfd
Balukh Konstantin <baluh.kostya@gmail.com>
2019-11-05 20:31:21 +0200

    Enum for supported databases

---
+++ DbQueryBuilder/Databases/DbType.cs
@@ -0,0 +1,12 @@
+﻿namespace DbQueryBuilder.Databases
+{
+    /// <summary>
+    /// Database type
+    /// </summary>
+    public enum DbType
+    {
+        MsSql = 0,
+        MySql = 1,
+        Firebird = 2
+    }
+}
