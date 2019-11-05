05762c1b950159f43b6fdb01a8d4fd9773de2483
Balukh Konstantin <baluh.kostya@gmail.com>
2019-11-05 23:14:58 +0200

    MsSqlQueryBuilderInsert

---
+++ DbQueryBuilder.MsSql/MsSqlQueryBuilderInsert.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace DbQueryBuilder.MsSql
+{
+    internal class MsSqlQueryBuilderInsert : IQueryBuilderInsert
+    {
+        private string _tableName;
+
+        private readonly MsSqlDatabase _dataBase;
+        private readonly IDbCommand _command;
+
+        private readonly Dictionary<string, object> _fieldValues = new Dictionary<string, object>();
+
+        internal MsSqlQueryBuilderInsert(MsSqlDatabase dataBase)
+        {
+            _dataBase = dataBase;
+            _command = dataBase.CreateCommand();
+        }
+
+        internal MsSqlQueryBuilderInsert(MsSqlDatabase dataBase, string tableName)
+        {
+            _dataBase = dataBase;
+            _command = dataBase.CreateCommand();
+            _tableName = tableName;
+        }
+
+        public void Add(string field, object value)
+        {
+            _command.Parameters.Add(CreateParameter(field, value));
+            _fieldValues.Add(field, value);
+        }
+
+        public IDbCommand GetCommand()
+        {
+            string fieldString = "";
+            string parameterString = "";
+
+            string insertFieldQuote = _dataBase.QueryFactory.CreateQueryQuotes().InsertFieldQuote;
+
+            // Формируем строки полей и параметров
+            foreach (KeyValuePair<string, object> kvp in _fieldValues)
+            {
+                string field = kvp.Key;
+
+                if (fieldString.Length > 1)
+                {
+                    fieldString += ", " + insertFieldQuote + field + insertFieldQuote;
+                    parameterString += ", @" + field;
+                }
+                else
+                {
+                    fieldString += insertFieldQuote + field + insertFieldQuote;
+                    parameterString += "@" + field;
+                }
+            }
+
+            // Формирование полного запроса
+            _command.CommandText = string.Format("INSERT INTO {0} ({1}) VALUES ({2})", _tableName, fieldString, parameterString);
+
+            return _command;
+        }
+
+        public void Into(string tableName)
+        {
+            _tableName = tableName;
+        }
+
+        private IDataParameter CreateParameter(string parameterName, object value)
+        {
+            IDataParameter parameter = _dataBase.CreateParameter();
+            parameter.ParameterName = "@" + parameterName;
+            parameter.Value = value;
+            return parameter;
+        }
+    }
+}
---
+++ DbQueryBuilder/IQueryBuilderInsert.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+
+namespace DbQueryBuilder
+{
+    public interface IQueryBuilderInsert
+    {
+        /// <summary>
+        /// Build block INTO
+        /// </summary>
+        /// <param name="tableName"></param>
+        void Into(string tableName);
+
+        /// <summary>
+        /// Adding into field value and encapsulate it in parameter
+        /// </summary>
+        /// <param name="field"></param>
+        /// <param name="value"></param>
+        void Add(string field, object value);
+
+        /// <summary>
+        /// Command for insert specific for concrete database
+        /// </summary>
+        /// <returns></returns>
+        IDbCommand GetCommand();
+    }
+}
